bf1e9804abf2e04a5c7689ec5cf72f4e772fdf56
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-03-03 00:01:01 +0900

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by ia on 2015/03/03.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
